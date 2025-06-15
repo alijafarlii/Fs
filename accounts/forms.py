e5e8d692272dcd0851a19fab2583e8f144545bdb
Emil <Emil@Emil-MacBook.local>
2025-06-15 13:08:27 +0400

    m

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from accounts.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class RegisterForm(UserCreationForm):
     password1 = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}))
@@ -8,7 +8,7 @@ class RegisterForm(UserCreationForm):
 
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'email', 'password1', 'password2', 'image', 'info',]
+        fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2', 'image', 'info',]
         widgets = {
             'first_name': forms.TextInput(attrs={'class':'form-control','placeholder':'First Name'}),
             'last_name': forms.TextInput(attrs={'class':'form-control','placeholder':'Last Name'}),
@@ -16,4 +16,15 @@ class RegisterForm(UserCreationForm):
             'image': forms.FileInput(attrs={'class':'form-control','placeholder':'Image'}),
             'info': forms.Textarea(attrs={'class':'form-control','placeholder':'Info'}),
             'email': forms.EmailInput(attrs={'class':'form-control','placeholder':'Email'}),
+        }
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Username'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class':'form-control', 'placeholder':'Password'}))
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+        widgets = {
+            'username': forms.TextInput(attrs={'class':'form-control','placeholder':'Username'}),
         }
--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,7 @@ from accounts.views import *
 app_name = 'accounts'
 
 urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register')
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', Login.as_view(), name='login'),
+    path('profile/<int:id>/', profile, name='profile')
 ]
